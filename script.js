112ab203e69ad92b182e26577050ec25535f2094
Emanuel <erestrepo123@cesde.net>
2026-05-02 09:56:41 -0500

    reto1

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let numero = document.getElementById('contador')
-let aumentar = document.getElementById('aumentar')
-let disminuir = document.getElementById('disminuir')
-let resetear = document.getElementById('resetear')
+let btnAumentar = document.getElementById('aumentar')
+let btnDisminuir = document.getElementById('disminuir')
+let btnResetear = document.getElementById('resetear')
+
+let cuenta = 0;
 
 const valoresVerdes = [6, 11, 24, 27] 
 let colorFondoOriginal = document.body.style.backgroundColor
@@ -14,10 +16,10 @@ function verificarColorFondo(valor) {
     }
 }
 
-aumentar.addEventListener('click', () => {
-    numero.innerHTML = parseInt(numero.innerHTML) + 1
-    let valor = parseInt(numero.innerHTML)
-    
+function aumentar() {
+    cuenta++;
+    document.getElementById("contador").innerHTML = cuenta;
+    let valor = cuenta;
     if (valor >= 0) {
         numero.style.color = 'black'
     }
@@ -27,30 +29,31 @@ aumentar.addEventListener('click', () => {
     if (valor == 6 || valor == 11 || valor == 24 || valor == 27) {
         numero.style.color = 'green'
     }
-    
     verificarColorFondo(valor)
-})
+}
 
-disminuir.addEventListener('click', () => {
-    numero.innerHTML = parseInt(numero.innerHTML) - 1
-    let valor = parseInt(numero.innerHTML)
-    
+function disminuir() {
+    cuenta--;
+    document.getElementById("contador").innerHTML = cuenta;
+    let valor = cuenta;
     if (valor <= -1) {
         numero.style.color = 'red'
     }
     if (valor < 10 && valor > 0) {
         numero.style.color = 'black'
     }
-    
     verificarColorFondo(valor)
-})
+}
 
-resetear.addEventListener('click', () => {
-    numero.innerHTML = 0
-    if (numero.innerHTML == 0) {
-        numero.style.color = 'green'
-    }
+function resetear() {
+    cuenta = 0;
+    document.getElementById("contador").innerHTML = cuenta;
+    numero.style.color = 'green'
     verificarColorFondo(0)
-})
+}
+
+btnAumentar.addEventListener('click', aumentar);
+btnDisminuir.addEventListener('click', disminuir);
+btnResetear.addEventListener('click', resetear);
 
 
